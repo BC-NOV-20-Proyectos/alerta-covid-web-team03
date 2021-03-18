f82ff924a0a1e97d0fb7d2bec94f63b19b6fe07c
ferreirarauldev <ferreirarauldev@gmail.com>
2021-03-17 22:53:07 -0300

    I add serach method for search institutions from the controller

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,9 @@
 class Institution < ApplicationRecord
+  def self.search(text)
+    if text.blank?  # blank? covers both nil and empty string
+      all
+    else
+      where('description LIKE ?', "%#{text}%")
+    end
+  end
 end
