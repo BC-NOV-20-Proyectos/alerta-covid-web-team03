d6c14aa6a8b5c468d6ade53dd19135b5733b9108
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-12 22:41:00 -0400

    Add a method for get quantity people who stayed in the same place with an infected or possible infected with covid.

---
+++ app/models/place_history.rb
@@ -0,0 +1,22 @@
+class PlaceHistory < ApplicationRecord
+  belongs_to :user
+  belongs_to :place
+  @from = 7.days.ago.beginning_of_day.strftime('%F')
+  @to = Date.today.strftime('%F')
+
+  def self.possible_infections(user_id)
+    query = <<-SQL 
+    SELECT u.id, u.name, u.lasname, u.email, pl.description
+    from place_histories ph
+    left join users u on ph.user_id = u.id
+    left join places pl on ph.place_id = pl.id
+    where ph.date between \'#{@from}\' and  \'#{@to}\'
+    and ph.place_id in  (select place_id 
+     from place_histories phi 
+     where phi.user_id = #{user_id }
+     and ph.date between \'#{@from}\' and  \'#{@to}\')
+    SQL
+    result = ActiveRecord::Base.connection.execute(query)
+  end
+  
+end
