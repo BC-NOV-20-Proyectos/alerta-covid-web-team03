cf19c67695fa604df369b3b019351cbcc4f6e57d
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-16 17:18:42 -0400

    simple seed for agree a admin user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a = User.new(email: 'a@gmail.com', password: 'a@gmail.com')
+a.add_role "admin"
+a.control_number= 1
+a.name="asd"
+a.lasname="dsa"
+a.save!
