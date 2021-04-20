da5f5754357db1e468a65db1c74e7a41bd2096b6
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-20 18:00:48 -0400

    add migration for relations for incident with test covid and symptoms

--- db/migrate/20210416222111_create_incidents.rb
+++ db/migrate/20210416222111_create_incidents.rb
@@ -1,10 +1,8 @@
 class CreateIncidents < ActiveRecord::Migration[6.1]
   def change
     create_table :incidents do |t|
-      t.date :from
-      t.references :user_history, null: false, foreign_key: true
-      t.references :test_covid, null: false, foreign_key: true
-
+      t.date :date_incident
+      t.references :user, null: false, foreign_key: true 
       t.timestamps
     end
   end
