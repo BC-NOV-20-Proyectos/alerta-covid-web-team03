326b8104d3d861ea28ffa04dc023e35e369e1076
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-20 18:00:11 -0400

    add relation for inicident

--- app/models/user_history.rb
+++ app/models/user_history.rb
@@ -1,4 +1,7 @@
 #UserHistory model
 class UserHistory < ApplicationRecord
-  belongs_to :user
+  belongs_to :incident, optional: true
+  has_many :user_history_details 
+
+  accepts_nested_attributes_for :user_history_details
 end
