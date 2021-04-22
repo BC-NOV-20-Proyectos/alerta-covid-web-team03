7c1724a1cac7a6e0977a1ea497ec85e16bc7a3b8
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-22 10:47:29 -0400

    add method on application helper for toastr_flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+
+  def toastr_flash
+    flash.each_with_object([]) do |(type, message), flash_messages|
+      type = 'info' if type == 'notice'
+      type = 'error' if type == 'alert'
+      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
+      flash_messages << text.html_safe if message
+    end.join("\n").html_safe
+end
+
   def resource_name
     :user
   end
