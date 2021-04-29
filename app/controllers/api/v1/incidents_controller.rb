7fe97fc8c89e2e465076b7aec43cfe1a21c2b16c
ferreirarauldev <ferreirarauldev@gmail.com>
2021-04-29 19:04:41 -0400

    Add two methods. alert_mail and set_aditional_data the first are a method for send email when an user report a symptom or a test results, and the last is for set user_id and current date for the new incident

--- app/controllers/api/v1/incidents_controller.rb
+++ app/controllers/api/v1/incidents_controller.rb
@@ -15,7 +15,9 @@ class API::V1::IncidentsController < API::V1::ApplicationAPIController
   # POST /incidents or /incidents.json
   def create
     @incident = Incident.new(incident_params)
+    set_aditional_data
       if @incident.save
+        alert_mail
         render json: @incident, status: 201
       else
        render json: @incident.errors, status: 401
@@ -34,6 +36,16 @@ class API::V1::IncidentsController < API::V1::ApplicationAPIController
       params.require(:incident).permit(:date_incident, :user_id, 
         :user_history_attributes => [:date, :user_history_details_attributes => [:symptom_id]],
         :test_covid_attributes => [:date_did, :date_get_result, :had_symptoms, :type_test, :result])
+    end
+
+    def set_aditional_data
+      @incident.user_id = current_user.id
+      @incident.date_incident = Time.now
+    end
+
+    def alert_mail
+      to = PlaceHistory.possible_infections(@incident.user_id).pluck("email")
+      NotificationMailer.alert_test(to)
     end
 
 end
